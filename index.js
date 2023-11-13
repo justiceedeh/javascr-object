dabf0c39f99cf22bdee0380175feb95ca736fdeb
justice2468 <edehozoemenam0@gmail.com>
2023-11-13 08:32:41 -1200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// question number 1
+
+let user = {}; // creates an empty object named "user"
+user.name = 'John'; // adds a property "name" with value "John" to "user"
+user.surname = 'Smith'; // adds a property "surname" with value "Smith" to "user"
+user.name = 'Pete'; // changes the value of the "name" property to "Pete"
+user.deleteProperty('name'); // deletes the "name" property from "user"
+
+//question Number 2
+
+
+function myFunction (chidi, justice, zulaikha){
+    return chidi + justice + zulaikha
+}
+console.log(myFunction(100, 160, 130))
